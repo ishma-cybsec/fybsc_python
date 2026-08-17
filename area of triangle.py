6ffadf1bbd89dd4095dc3fe0a43ba991776065a2
Ishma Deshmukh <ishmawarrior@gmail.com>
2026-08-17 10:14:13 +0530

    Add files via upload

---
+++ area of triangle.py
@@ -0,0 +1,11 @@
+#Header Info
+print("Area of Triangle")
+print("17/08/2026 | Python Practical 2")
+print("#Author: Ishma Rinchen\n")
+
+base = 10
+height = 5
+
+area = 0.5 * base * height
+
+print ("The area of: ", area)
