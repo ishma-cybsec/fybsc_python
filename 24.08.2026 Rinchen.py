05df5c97554aa4bacb04d31f34a904af721c24b3
Ishma Deshmukh <ishmawarrior@gmail.com>
2026-08-25 10:42:55 +0530

    Add files via upload

---
+++ 24.08.2026 Rinchen.py
@@ -0,0 +1,8 @@
+name = input ("Enter your Name")
+print ("Hello" , name)
+
+age = input ("Enter your Age")
+print ("Age" , age)
+
+marks = float (input ("Enter your Marks"))
+print ("Marks" , marks)
